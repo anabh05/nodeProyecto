96b772f6b353ed03042cb963f6059d877b9da5ef
anabh05 <anabh05@gmail.com>
2024-03-16 20:05:09 +0100

    Final con users

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,6 +1,21 @@
-const express = require('express')
+// const express = require('express')
+// const userRouter = express.Router();
+// const { register, login, logout } = require("../controllers/user.controller");
+// const { isAuth } = require("../middlewares/auth.middleware");
+
+// userRouter.post("/register", register);
+// userRouter.post("/login", login);
+// userRouter.post("/logout", [isAuth], logout);
+
+// module.exports = userRouter;
+const express = require("express");
 const userRouter = express.Router();
-const { register, login, logout } = require("../controllers/user.controller");
+const {
+  register,
+  login,
+  logout,
+
+} = require("../controllers/user.controller");
 const { isAuth } = require("../middlewares/auth.middleware");
 
 userRouter.post("/register", register);
