3e79669163676375275ab50d5f43ef810c9eef5e
anabh05 <anabh05@gmail.com>
2024-03-17 20:15:29 +0100

    final

--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,13 +1,3 @@
-// const express = require('express')
-// const userRouter = express.Router();
-// const { register, login, logout } = require("../controllers/user.controller");
-// const { isAuth } = require("../middlewares/auth.middleware");
-
-// userRouter.post("/register", register);
-// userRouter.post("/login", login);
-// userRouter.post("/logout", [isAuth], logout);
-
-// module.exports = userRouter;
 const express = require("express");
 const userRouter = express.Router();
 const {
